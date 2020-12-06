90ddee80b064af95b681a6dc6631636b5fe1ba21
Tanmaya0505 <tanmayarout54@gmail.com>
2020-12-06 20:43:21 +0530

    sql update

--- sql/interview .sql
+++ sql/interview .sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2019 at 08:58 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.2
+-- Generation Time: Dec 06, 2020 at 04:12 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.3.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `courses` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `status` tinyint(1) NOT NULL DEFAULT 1,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -41,9 +40,7 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'PHP', 1, '2019-04-10 12:42:50', '2019-04-10 12:42:50'),
-(2, 'MysQl', 1, '2019-04-10 12:43:19', '2019-04-10 12:43:19'),
-(3, 'jQuery', 1, '2019-04-10 12:43:29', '2019-04-10 12:43:29');
+(1, 'PHP', 1, '2020-11-07 02:49:06', '2020-11-07 02:49:06');
 
 -- --------------------------------------------------------
 
@@ -56,17 +53,10 @@ CREATE TABLE `customers` (
   `user_id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `comment` text COLLATE utf8mb4_unicode_ci,
+  `comment` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`id`, `user_id`, `name`, `email`, `comment`, `created_at`, `updated_at`) VALUES
-(2, 1, 'Trideep Dakua', 'trideepdakua@gmail.com', 'Test', '2019-04-11 10:46:11', '2019-04-11 10:46:11');
 
 -- --------------------------------------------------------
 
@@ -85,10 +75,10 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_04_10_164625_create_customers_table', 2),
-(4, '2019_04_10_173436_create_courses_table', 3);
+(5, '2014_10_12_000000_create_users_table', 1),
+(6, '2014_10_12_100000_create_password_resets_table', 1),
+(7, '2019_04_10_164625_create_customers_table', 1),
+(8, '2019_04_10_173436_create_courses_table', 1);
 
 -- --------------------------------------------------------
 
@@ -114,7 +104,7 @@ CREATE TABLE `users` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `photo` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -125,8 +115,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `photo`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Trideep Dakua', 'trideepdakua@gmail.com', '$2y$10$GRQV7q1moz2ZhS9msRITfezwpb40xUyk06ctworaOInSw0T0A7ejy', '7205821247', '2y10oX643insTNhjIZAw2m5bI7caey2DiobmznuahU9k0wp8f9pdPzC.jpg', 'Q2r1yyKeC6e0AinDBU6uI7XNhALPigqzIKmfbd8QwzYVA26zE75CrdegZ9rN', '2019-04-09 14:58:23', '2019-04-10 10:45:43'),
-(2, 'Kailash Dakua', 'kailashdakua@gmail.com', '$2y$10$aqBX6Ihrql.vjfCkeS8cQub5vGJ0JWLpCy8vPOB7U1L79F./HBZpG', '9861974755', '2y10vOluwQHNzFGYKJVb3AGkR4RvyPhR4w4bHU1szl272BiAc8TNt7wK.jpg', 'AfE4UjabWqVGKGpuT8gfU2FTyeDKFAx3jQLEEbXI6ch8ClkMoQVqdmcSS8EU', '2019-04-09 15:58:05', '2019-04-09 15:58:05');
+(1, 'tanmaya', 'tanmayarout54@gmail.com', '$2y$10$Y5RJ47clkIo/aAf3NDChMeQJKjHIjAvTbLFevpwn0f54zpXPV2lWC', '98765432', NULL, NULL, '2020-11-04 01:15:33', '2020-11-04 01:15:33'),
+(2, 'tanmaya', 'tanmayarour101@gmail.com', '$2y$10$XVcPrXg47fUGuY/OEpYPEOjdbH97AMP4IClRoBNvFYWbdSIX2UQce', '87654321', NULL, NULL, '2020-12-06 00:21:30', '2020-12-06 00:21:30'),
+(3, 'tanmaya', 'tanmaya54@gmail.com', '$2y$10$t4.un96Pc1E1uj3wsjhtXuqfhgcYl9n4rGDo67WfUxvTm0BSybtZu', '9040815030', NULL, 'zY3CQJHiOishvQM48jOogo7S3HeZMN2zevVdLSuDV2OhxamyIcS0LDF8OZnR', '2020-12-06 07:02:16', '2020-12-06 07:02:16'),
+(4, 'tan', 'tanmaya12@gmail.com', '$2y$10$YkO/99eVsvr.bx5Br9SfvO221GfRh5KxXZA7uW6HVZwUR3My1H5xm', '98765', NULL, 'kyhB02p4bPLKY6qPkNtU7xaPdBe3tWfOm5I8nhCorXopmbi8Vkk7HymDOuOi', '2020-12-06 08:42:50', '2020-12-06 08:42:50');
 
 --
 -- Indexes for dumped tables
@@ -173,25 +165,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
